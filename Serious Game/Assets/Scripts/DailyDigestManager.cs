97776e7364e757eec8f9dd253e7f64e2f2b2b0aa
yopox <louis.vignier@supelec.fr>
2020-03-05 11:30:06 +0100

    :art: :runner: News animation

--- Serious Game/Assets/Scripts/DailyDigestManager.cs
+++ Serious Game/Assets/Scripts/DailyDigestManager.cs
@@ -7,6 +7,9 @@ public class DailyDigestManager : MonoBehaviour {
         public GameObject dailyDigest;
         private static DailyDigestManager _instance;
         
+        private static readonly int Close = Animator.StringToHash("close");
+        private static readonly int Open = Animator.StringToHash("open");
+        
         
         public static DailyDigestManager Instance {
                 get {
@@ -19,12 +22,17 @@ public class DailyDigestManager : MonoBehaviour {
         /// This function is called by the exit button on the scene to close de DailyDigest. 
         /// </summary>
         public void CloseDd() {
-                dailyDigest.SetActive(false);
+                dailyDigest.GetComponent<Animator>().SetBool(Open, false);
+                dailyDigest.GetComponent<Animator>().SetBool(Close, true);
         }
 
         public void UpdateWithEvents(List<Event> dayEvents) {
                 foreach (var dayEvent in dayEvents) {
                         
                 }
+                
+                dailyDigest.GetComponent<Animator>().SetBool(Open, true);
+                dailyDigest.GetComponent<Animator>().SetBool(Close, false);
         }
+        
 }
