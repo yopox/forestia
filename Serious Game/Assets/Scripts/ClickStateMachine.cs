da3702850296236aca49a650f3c7a5d6ebd84429
Quentin Michel <quentinmichel69110@gmail.com>
2020-03-05 11:19:38 +0100

    ClickStateMachine

---
+++ Serious Game/Assets/Scripts/ClickStateMachine.cs
@@ -0,0 +1,50 @@
+public enum ClickState {
+    Forest,
+    InGameMenu,
+    MoveUnit,
+    DailyDigest,
+}
+
+public class ClickStateMachine {
+    public ClickState state { get; set; }
+
+    public ClickStateMachine() {
+        state = ClickState.DailyDigest;
+    }
+
+    public void MoveAction() {
+        if (state == ClickState.Forest) {
+            state = ClickState.MoveUnit;
+        }
+    }
+
+    public void UnitMoved() {
+        if (state == ClickState.MoveUnit) {
+            state = ClickState.Forest;
+        }
+    }
+
+    public void ReturnToGame() {
+        if (state == ClickState.InGameMenu) {
+            state = ClickState.Forest;
+        }
+    }
+
+    public void BurgerClick() {
+        if (state == ClickState.Forest || state == ClickState.MoveUnit) {
+            state = ClickState.InGameMenu;
+        }
+    }
+
+    public void NewTurn() {
+        if (state == ClickState.Forest) {
+            state = ClickState.DailyDigest;
+        }
+    }
+
+    public void Dismiss() {
+        if (state == ClickState.DailyDigest) {
+            state = ClickState.Forest;
+        }
+    }
+}
