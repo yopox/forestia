ecfa0adb96c644c1a080c582719063eb05241808
yopox <louis.vignier@supelec.fr>
2020-03-06 11:14:59 +0100

    :pencil: :book: Updated glossary

--- Serious Game/Assets/Scripts/GlossaryManager.cs
+++ Serious Game/Assets/Scripts/GlossaryManager.cs
@@ -19,14 +19,24 @@ public class GlossaryManager : MonoBehaviour {
     public Text page;
     private int _pageNb;
 
-    private readonly string[] _categories = new string[] {
+    private readonly string[] _categories = {
         "Amazon Rainforest",
-        "Climate Change"
+        "Challenges",
+        "Climate Change",
+        "Food & Farming",
+        "Bio piracy",
+        "Eat sustainably",
+        "Reduce plastic"
     };
 
-    private readonly string[] _descriptions = new string[] {
-        "The Amazon is burning. Around 73,000 km2 of land was burnt in the Amazon rainforest of Brazil in 2019.\n\nAnd they continue into 2020. Soaring deforestation is one of the main causes of these fires. We must fight the causes and protect this precious place. - WWF",
-        "The Amazon is at the heart of global climate concerns.\n\nNot only does the destruction of rainforests add to carbon dioxide in the atmosphere, it creates a 'positive feedback loop', where increased deforestation causes a rise in temperatures which in turn can bring about a drying of tropical forests and increase the risk of forest fires. - WWF"
+    private readonly string[] _descriptions = {
+        "The Amazon is burning. Around 73,000 km2 of land was burnt in the Amazon rainforest of Brazil in 2019.\n\nAnd they continue into 2020. Soaring deforestation is one of the main causes of these fires. We must fight the causes and protect this precious place.\n- WWF",
+        "Huge areas of rainforest are destroyed by clearing for farming, timber, roads, hydropower dams, mining, house-building or other development.\n\nThe problem is it’s often seen as more economically worthwhile to cut the forest down than to keep it standing.\n- WWF",
+        "The Amazon is at the heart of global climate concerns.\n\nNot only does the destruction of rainforests add to carbon dioxide in the atmosphere, it creates a 'positive feedback loop', where increased deforestation causes a rise in temperatures which in turn can bring about a drying of tropical forests and increase the risk of forest fires. - WWF",
+        "Rising global demand for food, especially meat, has led to Brazil becoming the world’s biggest beef exporter, and the second-biggest exporter of soy beans, mainly used for livestock feed.\n\nMore and more forests are being removed to make way for grazing land or soy plantations.\n- WWF",
+        "People take plants and animals from the Amazon to sell abroad as pets, food, and medicine. Trade in these animals leads to declines in wild populations, affecting animals already threatened by habitat destruction and pollution.\n\nAccording to Wikipedia, in 2008, it was estimated that the illegal trade in wildlife is worth at least US$5 billion, and may potentially total in excess of US$20 billion annually.\n- rainforestcruises.com",
+        "The food we eat causes half of all global deforestation. This is a major driver of climate change and loss of wildlife – with the UK supply chain linked to the extinction of 33 species already.\n\nIt’s time to rethink the way we eat to ensure there's enough space for nature and people to thrive.\n- WWF",
+        "Around 8 million tonnes of plastic enter our oceans every year – and by 2050 there could be more plastic in our seas than fish.\n\nOur oceans are a vital defence against climate change as they soak up carbon, but they need to be healthy for that to work.\n- WWF"
     };
     
     private static readonly int Open = Animator.StringToHash("open");
