b4af6083b8e4edb77d244319f5cb19cfb74b711f
tipunchetrhum <thibaultdeboutray@gmail.com>
2020-03-11 11:42:29 +0100

    Revert ":pencil: Shorter events name"
    This reverts commit 3684425ca489d0b8303c17e890f0cecc7c0343b0.

--- Serious Game/Assets/Scripts/Events/FarmersEvent.cs
+++ Serious Game/Assets/Scripts/Events/FarmersEvent.cs
@@ -2,7 +2,7 @@ using GameVariables;
 
 namespace Events {
     public class FarmersEvent : Event {
-        public FarmersEvent() : base("Farmers deforested", "Farmers are deliberately starting blazes in their efforts to clear land for crops or livestock.") {
+        public FarmersEvent() : base("Farmers caused deforestation", "Farmers are deliberately starting blazes in their efforts to clear land for crops or livestock.") {
             AddInfluence(new AbsoluteInfluence(typeof(Biodiversity), -25));
         }
     }
--- Serious Game/Assets/Scripts/Events/GoldDiggersEvent.cs
+++ Serious Game/Assets/Scripts/Events/GoldDiggersEvent.cs
@@ -3,7 +3,7 @@ using Tiles;
 
 namespace Events {
     public class GoldDiggersEvent : Event {
-        public GoldDiggersEvent() : base("Illegal gold mining", "Illegal gold mining has led to acres of rainforest being destroyed.") {
+        public GoldDiggersEvent() : base("Illegal gold mining in Forestia", "Illegal gold mining has led to acres of rainforest being destroyed.") {
             AddInfluence(new AbsoluteInfluence(typeof(Criminality), 10));
         }
     }
